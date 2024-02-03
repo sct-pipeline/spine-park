85202a6f64775c18845c8757033f1fc7090b1671
jcohenadad <jcohen@polymtl.ca>
2024-02-02 21:31:01 -0500

    Added docstrings, added output path

--- convert_to_bids.py
+++ convert_to_bids.py
@@ -5,6 +5,22 @@ import re
 
 
 def extract_patient_id(dirname):
+    """Converts the directory name to a BIDS-compatible patient ID.
+
+    Examples of directory names and conversions:
+    2023_10_02_DEV2_206_01_ICEBERG_ME_Sujet10 -> sub-DEV206Sujet10
+    2020_09_16_ICEBERG_LM_166_V3_M -> sub-LM166
+    2020_10_28_ICEBERG_LC_164_V3_M -> sub-LC164
+    2020_11_18_ICEBERG_BJ_170_V3_M -> sub-BJ170
+    2020_10_29_ICEBERG_BB_277_V1_M -> sub-BB277
+    2023_10_20_DEV2_214_01_ICEBERG_ME_Sujet12 -> sub-DEV214Sujet12
+
+    Args:
+        dirname (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     # Check if subject is healthy control or patient
     if "DEV2" in dirname:
         # Healthy control
@@ -19,30 +35,17 @@ def extract_patient_id(dirname):
     return patient_id
 
 
-# Test the function with a set of directory names
-test_dirs = [
-    "2023_10_02_DEV2_206_01_ICEBERG_ME_Sujet10",
-    "2020_09_16_ICEBERG_LM_166_V3_M",
-    "2020_10_28_ICEBERG_LC_164_V3_M",
-    "2020_11_18_ICEBERG_BJ_170_V3_M",
-    "2020_10_29_ICEBERG_BB_277_V1_M",
-    "2023_10_20_DEV2_214_01_ICEBERG_ME_Sujet12"
-]
-
-for dir_name in test_dirs:
-    print(f"{dir_name} -> {extract_patient_id(dir_name)}")
-
-
 def convert_mri_to_bids(root_dir):
     for patient_dir in os.listdir(root_dir):
         patient_path = os.path.join(root_dir, patient_dir)
         if os.path.isdir(patient_path):
             bids_patient_id = extract_patient_id(patient_dir)
+            print(f"{patient_dir} -> {bids_patient_id}")
             if not bids_patient_id:
                 print(f"Could not determine BIDS patient ID for directory: {patient_dir}")
                 continue  # Skip directory if patient ID cannot be determined
             
-            bids_path = os.path.join(patient_path, bids_patient_id)
+            bids_path = os.path.join(path_out, bids_patient_id)
             os.makedirs(bids_path, exist_ok=True)
 
             # Counters for DWI chunks
@@ -70,13 +73,44 @@ def convert_mri_to_bids(root_dir):
                                         shutil.copy(os.path.join(dirpath, dwi_file), os.path.join(bids_path, bids_subfolder, new_filename.replace('.nii.gz', ext)))
                                 dwi_chunk_counter += 1
 
+
 def determine_scan_type_and_bids_path(filename, patient_id, dwi_chunk_counter):
-    # Scans matching and renaming logic unchanged
-    pass
+    """Determines the type of scan and sets the BIDS path.
+
+    Args:
+        filename (_type_): _description_
+        patient_id (_type_): _description_
+        dwi_chunk_counter (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if "T2_SAG" in filename:
+        return f"sub-{patient_id}_T2.nii.gz", "anat"
+    elif "DTI_64DIR" in filename:
+        return f"sub-{patient_id}_chunk-{dwi_chunk_counter}_DWI.nii.gz", "dwi"
+    elif "T1_SAG_MT_FL3D" in filename:
+        return f"sub-{patient_id}_mt-on_MTS.nii.gz", "anat"
+    elif "T1_SAG_FL3D" in filename:
+        return f"sub-{patient_id}_mt-off_MTS.nii.gz", "anat"
+    elif "mp2rage_sag_p3_1mm_iso_T1" in filename:
+        return f"sub-{patient_id}_T1map.nii.gz", "anat"
+    elif "mp2rage_sag_p3_1mm_iso_UNI" in filename:
+        return f"sub-{patient_id}_UNIT1.nii.gz", "anat"
+    return None, None
+
 
 def zip_and_move_nifti(src_path, dest_path):
-    # NIfTI zipping and moving logic unchanged
-    pass
+    """Zips and moves a NIfTI file.
 
-root_directory = "/Users/julien/temp/20240122_Lydia/CENIR_ICEBERG_spine"  # Replace with the path to the root directory
+    Args:
+        src_path (_type_): _description_
+        dest_path (_type_): _description_
+    """
+    with open(src_path, 'rb') as f_in, gzip.open(dest_path, 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+
+root_directory = "/Users/julien/temp/20240122_Lydia/CENIR_ICEBERG_spine"
+path_out = "/Users/julien/temp/20240122_Lydia/CENIR_ICEBERG_spine_BIDS"
 convert_mri_to_bids(root_directory)
